cac89495fd47cdfd99f4100eb7e26d50d08d50cd
chaeheedongs <qiip91@gmail.com>
2025-04-23 23:32:51 +0900

    feature/05-filter | yml에 filter 구동 설정 기능 추가

--- src/main/java/com/enjoy/Spring/config/filters/FilterConfig.java
+++ src/main/java/com/enjoy/Spring/config/filters/FilterConfig.java
@@ -1,7 +1,7 @@
 package com.enjoy.Spring.config.filters;
 
 import com.enjoy.Spring.config.filters.filter.LoggingFilter;
-import com.enjoy.Spring.config.filters.types.UrlPattern;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +9,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
+    @Value("${project.switch.filter}")
+    private boolean filterSwitch;
+
     @Bean
     public FilterRegistrationBean loggingFilter() {
         final FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setEnabled(filterSwitch);
         registration.setFilter(new LoggingFilter());
-        registration.addUrlPatterns(UrlPattern.ALL.getCode());
         registration.setName("loggingFilter");
         registration.setOrder(1);
+
         return registration;
     }
 }
