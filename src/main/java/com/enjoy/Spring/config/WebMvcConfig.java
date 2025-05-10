5193b381e8d2ba4473d91fa5bca765548f3a8ae4
chaeheedongs <qiip91@gmail.com>
2025-05-10 17:03:22 +0900

    feature/09-argument-resolver | 어노테이션 기반 Argument Resolver를 WebMvcConfigurer에 등록

--- src/main/java/com/enjoy/Spring/config/WebMvcConfig.java
+++ src/main/java/com/enjoy/Spring/config/WebMvcConfig.java
@@ -1,16 +1,26 @@
 package com.enjoy.Spring.config;
 
 import com.enjoy.Spring.config.interceptors.SessionInterceptor;
+import com.enjoy.Spring.config.resolvers.arguments.AnnotationArgumentResolver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${project.switch.interceptor}")
     private boolean interceptorSwitch;
+
+    private final AnnotationArgumentResolver annotationArgumentResolver;
+
+    public WebMvcConfig(final AnnotationArgumentResolver annotationArgumentResolver) {
+        this.annotationArgumentResolver = annotationArgumentResolver;
+    }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
@@ -19,5 +29,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                     .addPathPatterns("/**")
                     .excludePathPatterns("/exclude/**");
         }
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(annotationArgumentResolver);
     }
 }
